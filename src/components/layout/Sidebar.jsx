0942184f42844cfdf93d367688dcff97f48b20b7
mahmoudGalal67 <mahmoudfarag@gmail.com>
2026-02-01 08:30:14 +0200

    fsd

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -26,7 +26,7 @@ export default function Sidebar() {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-    window.location.href = "/signIn";
+    window.location.href = "/";
   };
 
 
